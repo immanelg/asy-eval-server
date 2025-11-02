9188b657d1a76b3f5e23b11fd5891794e0768ef7
immanelg <119798691+immanelg@users.noreply.github.com>
2025-11-02 19:43:28 +0300

    structured logging

---
+++ log.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"sync/atomic"
+	"context"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+)
+var slogger *slog.Logger
+
+type slogHandler struct {
+	slog.Handler
+}
+
+func (h slogHandler) Handle(ctx context.Context, record slog.Record) error {
+	if requestID, ok := ctx.Value(contextKeyRequestID).(string); ok { //not inside request
+		record.Add("rid", slog.StringValue(requestID))
+	}
+	return h.Handler.Handle(ctx, record)
+}
+
+const contextKeyRequestID = "requestID"
+
+func init() {
+	baseHandler := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelInfo,
+	})
+	slogger = slog.New(slogHandler{baseHandler})
+}
+
+var nextReqId atomic.Uint64
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		requestID := fmt.Sprintf("%d", nextReqId.Load())
+		nextReqId.Add(1)
+
+		ctx := context.WithValue(r.Context(), contextKeyRequestID, requestID)
+		r = r.WithContext(ctx)
+
+		slogger.InfoContext(ctx, "start request", "method", r.Method, "path", r.URL.Path)
+		w.Header().Set("Request-ID", requestID)
+		defer slogger.InfoContext(ctx, "done request")
+
+		next.ServeHTTP(w, r)
+	})
+}
--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	// "fmt"
-	// "html"
 	"flag"
 	"io"
 	"log"
@@ -13,98 +11,101 @@ import (
 	"strings"
 )
 
-func compile() {
+func serveHtml(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
 }
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "index.html")
-	})
 
-	http.HandleFunc("/eval", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodPost {
-			// data, err := io.ReadAll(r.Body)
-			// if err != nil {
-			// 	log.Fatal(err)
-			// }
-			// expr := string(data)
-			// log.Println("expr:", expr)
+func handleCompilation(w http.ResponseWriter, r *http.Request) {
+	// data, err := io.ReadAll(r.Body)
+	// if err != nil {
+	//  logger.ErrorContext(r.Context(), "read body failed", "error", err)
+	// }
+	// expr := string(data)
+	// logger.InfoContext(r.Context(), "expr", "expr", expr)
 
-			tmpdir, err := os.MkdirTemp("", "asy-eval-1")
-			if err != nil {
-				log.Fatal(err)
-			}
-			log.Println("tmpdir:", tmpdir)
-			defer os.RemoveAll(tmpdir)
+	tmpdir, err := os.MkdirTemp("", "asy-eval-1")
+	if err != nil {
+		slogger.ErrorContext(r.Context(), "create temp dir failed", "error", err)
+	}
+	slogger.InfoContext(r.Context(), "create tmpdir", "path", tmpdir)
+	defer os.RemoveAll(tmpdir)
 
-			inpf, err := os.Create(filepath.Join(tmpdir, "input.asy"))
-			if err != nil {
-				log.Fatal(err)
-			}
-			defer func() {
-				if err := inpf.Close(); err != nil {
-					log.Fatal(err)
-				}
-			}()
-
-			buf := make([]byte, 512)
-			for {
-				n, err := r.Body.Read(buf)
-				if err != nil && err != io.EOF {
-					log.Fatal(err)
-				}
-				if n == 0 {
-					break
-				}
-
-				if _, err := inpf.Write(buf[:n]); err != nil {
-					log.Fatal(err)
-				}
-			}
-
-			accept := r.Header.Get("Accept")
-			var fmt string = "svg"
-			// fmts := make([]string, 0, 4)
-			// if strings.Contains(accept, "application/pdf") { fmts = append(fmts, "pdf") }
-			// if strings.Contains(accept, "application/svg+xml") { fmts = append(fmts, "svg") }
-			// if strings.Contains(accept, "image/png") { fmts = append(fmts, "png") }
-
-			if strings.Contains(accept, "application/pdf") { fmt = "pdf" 
-			} else if strings.Contains(accept, "application/svg+xml") { fmt = "svg" 
-			} else if strings.Contains(accept, "image/png") { fmt = "png" }
-
-			cmd := exec.Command("asy", "input.asy", "-safe", "-f", fmt, "-o", "output")
-			log.Print("eval command line: ", strings.Join(cmd.Args, " "))
-			cmd.Dir = tmpdir
-			outputb, err := cmd.CombinedOutput()
-			log.Print("output: ", string(outputb))
-
-			outfilepath := filepath.Join(tmpdir, "output."+fmt)
-
-			const compilerErrorMimeType =  "text/vnd.asy-compiler-error"
-			if err != nil {
-				log.Print("eval error: ", err)
-				// TODO: maybe parse errors to json server side
-				w.Header().Add("Content-Type", compilerErrorMimeType)
-				w.WriteHeader(200)
-				w.Write(outputb)
-			} else {
-				if _, err := os.Stat(outfilepath); err != nil {
-					log.Print("cannot stat output file to serve it: ", err)
-					w.Header().Add("Content-Type", compilerErrorMimeType)
-					w.WriteHeader(204) // https://http.cat/204
-					w.Write([]byte("no output"))
-				} else {
-					http.ServeFile(w, r, outfilepath)
-				}
-			}
-		} else {
-			http.NotFound(w, r)
+	inpf, err := os.Create(filepath.Join(tmpdir, "input.asy"))
+	if err != nil {
+		slogger.ErrorContext(r.Context(), "create input file failed", "error", err)
+	}
+	slogger.InfoContext(r.Context(), "create input.asy")
+	defer func() {
+		if err := inpf.Close(); err != nil {
+			slogger.ErrorContext(r.Context(), "close input file failed", "error", err)
 		}
-	})
+	}()
+
+	buf := make([]byte, 512)
+	for {
+		n, err := r.Body.Read(buf)
+		if err != nil && err != io.EOF {
+			slogger.ErrorContext(r.Context(), "read body failed", "error", err)
+		}
+		if n == 0 {
+			break
+		}
+
+		if _, err := inpf.Write(buf[:n]); err != nil {
+			slogger.ErrorContext(r.Context(), "write to input file failed", "error", err)
+		}
+	}
+
+	accept := r.Header.Get("Accept")
+	var fmt string = "svg"
+	// fmts := make([]string, 0, 4)
+	// if strings.Contains(accept, "application/pdf") { fmts = append(fmts, "pdf") }
+	// if strings.Contains(accept, "application/svg+xml") { fmts = append(fmts, "svg") }
+	// if strings.Contains(accept, "image/png") { fmts = append(fmts, "png") }
+
+	if strings.Contains(accept, "application/pdf") {
+		fmt = "pdf"
+	} else if strings.Contains(accept, "application/svg+xml") {
+		fmt = "svg"
+	} else if strings.Contains(accept, "image/png") {
+		fmt = "png"
+	}
+
+	cmd := exec.Command("asy", "input.asy", "-safe", "-f", fmt, "-o", "output")
+	cmd.Dir = tmpdir
+	slogger.InfoContext(r.Context(), "exec asy cmd", "args", cmd.Args)
+
+	outputb, err := cmd.CombinedOutput()
+	slogger.InfoContext(r.Context(), "output", "output", string(outputb))
+	const compilerErrorMimeType = "text/vnd.asy-compiler-error"
+	if err != nil {
+		slogger.WarnContext(r.Context(), "exec asy cmd error", "error", err)
+		// TODO: maybe parse errors to json server side
+		w.Header().Add("Content-Type", compilerErrorMimeType)
+		w.WriteHeader(200)
+		w.Write(outputb)
+	} else {
+		outfilepath := filepath.Join(tmpdir, "output."+fmt)
+		if _, err := os.Stat(outfilepath); err != nil {
+			slogger.WarnContext(r.Context(), "cannot stat output file to serve it", "error", err)
+			w.Header().Add("Content-Type", compilerErrorMimeType)
+			w.WriteHeader(204) // https://http.cat/204
+			w.Write([]byte("no output"))
+		} else {
+			http.ServeFile(w, r, outfilepath)
+		}
+	}
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.Handle("GET /", loggingMiddleware(http.HandlerFunc(serveHtml)))
+	mux.Handle("POST /eval", loggingMiddleware(http.HandlerFunc(handleCompilation)))
 
 	var addr = "localhost:5001"
-	flag.StringVar(&addr,"addr", "localhost:5001", "address to use")
+	flag.StringVar(&addr, "addr", "localhost:5001", "address to use")
 	flag.Parse()
-	log.Print("listening on: ", addr)
-	log.Fatal(http.ListenAndServe(addr, nil))
+	slogger.Info("starting server", "addr", addr)
+	log.Fatal(http.ListenAndServe(addr, mux))
 }
